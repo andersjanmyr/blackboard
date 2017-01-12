857ccee9730a41e0894f5c1f4655565f95db436c
Anders Janmyr <anders@janmyr.com>
2017-01-12 09:49:19 -0800

    Tweaked handlers and changed tablename

--- main.js
+++ main.js
@@ -5,92 +5,70 @@ var appId = 'amzn1.ask.skill.61d7c258-1de1-48af-be3d-36797fe49007';
 exports.handler = function(event, context, callback) {
     var alexa = Alexa.handler(event, context);
     alexa.appId = appId;
-    alexa.dynamoDBTableName = 'notes';
-    alexa.registerHandlers(newSessionHandlers, guessModeHandlers, startGameHandlers, guessAttemptHandlers);
+    alexa.dynamoDBTableName = 'BlackboardNotes';
+    alexa.registerHandlers(topLevelHandlers, noteHandlers);
     alexa.execute();
 };
 
 var states = {
-    GUESSMODE: '_GUESSMODE', // User is trying to guess the number.
-    STARTMODE: '_STARTMODE'  // Prompt the user to start or restart the game.
+    NOTEMODE: '_NOTEMODE' // User has asked to add a note
 };
 
-var newSessionHandlers = {
-    'NewSession': function() {
-        if(Object.keys(this.attributes).length === 0) {
-            this.attributes['endedSessionCount'] = 0;
-            this.attributes['gamesPlayed'] = 0;
-        }
-        this.handler.state = states.STARTMODE;
-        this.emit(':ask', 'Welcome to High Low guessing game. You have played '
-            + this.attributes['gamesPlayed'].toString() + ' times. would you like to play?',
-            'Say yes to start the game or no to quit.');
-    }
-};
-
-var startGameHandlers = Alexa.CreateStateHandler(states.STARTMODE, {
-    'NewSession': function () {
-        this.emit('NewSession'); // Uses the handler in newSessionHandlers
-    },
-    'AMAZON.HelpIntent': function() {
-        var message = 'I will think of a number between zero and one hundred, try to guess and I will tell you if it' +
-            ' is higher or lower. Do you want to start the game?';
-        this.emit(':ask', message, message);
-    },
-    'AMAZON.YesIntent': function() {
-        this.attributes["guessNumber"] = Math.floor(Math.random() * 100);
-        this.handler.state = states.GUESSMODE;
-        this.emit(':ask', 'Great! ' + 'Try saying a number to start the game.', 'Try saying a number.');
-    },
-    'AMAZON.NoIntent': function() {
-        this.emit(':tell', 'Ok, see you next time!');
-    },
-    'SessionEndedRequest': function () {
-        console.log('session ended!');
-        this.attributes['endedSessionCount'] += 1;
-        this.emit(':saveState', true);
-    },
-    'Unhandled': function() {
-        var message = 'Say yes to continue, or no to end the game.';
-        this.emit(':ask', message, message);
-    }
-});
-
-var guessModeHandlers = Alexa.CreateStateHandler(states.GUESSMODE, {
-    'NewSession': function () {
+var topLevelHandlers = {
+    'LaunchRequest': function() {
         this.handler.state = '';
-        this.emitWithState('NewSession'); // Equivalent to the Start Mode NewSession handler
-    },
-    'NumberGuessIntent': function() {
-        var guessNum = parseInt(this.event.request.intent.slots.number.value);
-        var targetNum = this.attributes["guessNumber"];
-        console.log('user guessed: ' + guessNum);
-
-        if(guessNum > targetNum){
-            this.emit('TooHigh', guessNum);
-        } else if( guessNum < targetNum){
-            this.emit('TooLow', guessNum);
-        } else if (guessNum === targetNum){
-            // With a callback, use the arrow function to preserve the correct 'this' context
-            this.emit('JustRight', () => {
-                this.emit(':ask', guessNum.toString() + 'is correct! Would you like to play a new game?',
-                'Say yes to start a new game, or no to end the game.');
-        })
-        } else {
-            this.emit('NotANum');
-        }
+        this.emit(':ask', 'Welcome to Blackboard. Would you like me to add, read, or '
+            + 'erase a note?')
     },
     'AMAZON.HelpIntent': function() {
-        this.emit(':ask', 'I am thinking of a number between zero and one hundred, try to guess and I will tell you' +
-            ' if it is higher or lower.', 'Try saying a number.');
+        var message = 'I can add, read, or erase a note. What would you like to do?';
+        this.emit(':ask', message, message);
     },
     'SessionEndedRequest': function () {
         console.log('session ended!');
-        this.attributes['endedSessionCount'] += 1;
         this.emit(':saveState', true);
     },
+    'AddIntent': function(name) {
+        if (!name) name = 'everyone';
+        this.handler.state = state.NOTEMODE;
+        var message = 'Record note for ' + name;
+        this.emit(':ask', message, message)
+    },
+    'ReadIntent': function(name) {
+        if (!name) name = 'everyone';
+        this.handler.state = '';
+        var note;
+        if (this.attributes[name])
+            note = 'Note for ' + name +': '  + this.attributes[name];
+        else
+           note = 'There is no note for ' + name;
+        console.log('name:', name, 'note:', note);
+        this.emit(':tellWithCard', note, 'Blackboard', note)
+    },
+    'EraseIntent': function(name) {
+        if (!name) name = 'everyone';
+        this.handler.state = '';
+        delete this.attributes[name];
+        console.log('deleted note for name:', name);
+    },
     'Unhandled': function() {
-        this.emit(':ask', 'Sorry, I didn\'t get that. Try saying a number.', 'Try saying a number.');
+        var message = 'I can add, read, or erase a note. What would you like to do?';
+        this.emit(':ask', message, message);
+    }
+};
+
+var noteHandlers = Alexa.CreateStateHandler(states.NOTE, {
+    'NoteIntent': function(note) {
+        if (!note) {
+            console.log('No note')
+        }
+        this.handler.state = '';
+        this.attributes[name] = note;
+        console.log('added note for name:', name, note);
+    },
+    'Unhandled': function() {
+        var message = 'This could not possibly happen';
+        this.emit(':ask', message, message);
     }
 });
 
